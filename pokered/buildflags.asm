6c63ec97c00a0ef2c1cf16ffc2eb4653ba93db00
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2019-02-07 03:46:37 -0300

    Add a temporary build flag

--- pokered/buildflags.asm
+++ pokered/buildflags.asm
@@ -10,3 +10,5 @@ _RED EQU (_BUILD_FLAGS & 1) == 0
 _BLUE EQU (_BUILD_FLAGS & 1) == 1
 _GREEN EQU 0 ;we don't build Green
 _GIRL EQU (_BUILD_FLAGS & 2) == 2
+
+_ITEMAPI EQU 1 ;temporary - we'll set up the conditional later
