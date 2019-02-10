5401111841a83b453316b8991f6af55a5df6ed13
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2019-02-10 02:34:26 -0300

    Even more sanity here

--- pokered/main.asm
+++ pokered/main.asm
@@ -619,7 +619,6 @@ TangelaPicBack::      INCBIN "pic/monback/tangelab.pic"
 SECTION "Battle (BANK 9)", ROMX
 
 INCLUDE "engine/battle/print_type.asm"
-INCLUDE "engine/battle/save_trainer_name.asm"
 INCLUDE "engine/battle/moveEffects/focus_energy_effect.asm"
 
 
--- pokered/text.asm
+++ pokered/text.asm
@@ -40,7 +40,7 @@ _CardKeyFailText::
 	done
 
 _TrainerNameText::
-	TX_RAM wcd6d
+	TX_RAM wTrainerName
 	text ": @@"
 
 _NoNibbleText::
